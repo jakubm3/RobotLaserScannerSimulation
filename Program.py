a5cd09aea4aea37942464a4b09507aa05cb3f691
Jakub Mierzejewski <jakubmierzejewski3@icloud.com>
2024-12-16 12:22:40 +0100

    Added Line class, with method implementing Bresenham's Algorithm

--- Program.py
+++ Program.py
@@ -1,11 +1,51 @@
-def BresenhamLine(x1, y1, x2, y2):
-    pass
-
-
 class Point:
     def __init__(self, x, y):
+        if not isinstance(x, int) or not isinstance(y, int):
+            raise TypeError("Coordinates must be integers")
         self.x = x
         self.y = y
+
+
+class Line:
+    def __init__(self, start: Point, end: Point):
+        self.start = start
+        self.end = end
+        self.points = []
+
+    def LinePoints(self):
+        swapped_axes = False
+        distance_x = self.end.x - self.start.x
+        distance_y = self.end.y - self.start.y
+        x1, y1 = self.start.x, self.start.y
+        x2, y2 = self.end.x, self.end.y
+        slope = distance_y / distance_x
+
+        if abs(distance_y) > abs(distance_x):
+            x1, y1, x2, y2 = y1, x1, y2, x2
+            distance_x, distance_y = distance_y, distance_x
+            slope = distance_y / distance_x
+            swapped_axes = True
+
+        if x1 > x2:
+            x1, x2 = x2, x1
+            y1, y2 = y2, y1
+
+        error = None
+        y0 = y1
+
+        for x in range(x1, x2+1):
+            if swapped_axes:
+                self.points.append(Point(y0, x))
+            else:
+                self.points.append(Point(x, y0))
+
+            error += slope
+
+            if abs(error) >= 0.5:
+                y0 += 1 if y2 > y1 else -1
+                error -= 1.0 if error > 0 else -1.0
+
+        return self.points[::-1] if swapped_axes else self.points
 
 
 def LoadImage(image_path):
