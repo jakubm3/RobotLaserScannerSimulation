8b6a116449ac5dbedc4c0ad08cb0b9f0b6edb17b
Jakub Mierzejewski <jakubmierzejewski3@icloud.com>
2024-12-16 10:37:32 +0100

    Implemented program outline with placeholder functions.

---
+++ Program.py
@@ -0,0 +1,22 @@
+def BresenhamLine(x1, y1, x2, y2):
+    pass
+
+
+def LoadImage(image_path):
+    pass
+
+
+def LoadParmeters(file_path):
+    pass
+
+
+def DrawLine(image, x1, y1, x2, y2):
+    pass
+
+
+def FindObstacle(image, line):
+    pass
+
+
+def FindLineEndingPoints(x1, y1, angle):
+    pass
