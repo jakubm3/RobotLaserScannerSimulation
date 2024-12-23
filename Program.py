de74fb9f28c89025176e6f88f7a903cc63b815c9
Jakub Mierzejewski <jakubmierzejewski3@icloud.com>
2024-12-23 23:02:22 +0100

    Added main function, giving the program its main functionality.

--- Program.py
+++ Program.py
@@ -132,3 +132,36 @@ def SimulateLaserScanner(image_path, params_path):
         DrawLine(image_array, line_points, line_length)
 
     return image_array, line_lengths
+
+
+def main():
+    environment_path = "otoczenie.png"
+    params_path = "parametry.txt"
+
+    try:
+        print("Starting simulation")
+        image_array, line_lengths = (
+            SimulateLaserScanner(environment_path, params_path))
+
+        simulation_image = Image.fromarray(image_array)
+        simulation_image.save("symulacja.png")
+        print("Simulation image saved as 'symulacja.png'")
+
+        with open("wyniki.txt", "w") as file:
+            file.write('\n'.join(map(str, line_lengths)))
+        print("Line lengths saved to 'wyniki.txt'")
+
+    except WrongExtensionError as e:
+        print(f"Error: {e}")
+    except OutOfRangeError as e:
+        print(f"Error: {e}")
+    except ValueError as e:
+        print(f"Error: {e}")
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
